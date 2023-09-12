582fc69d40e2f5d6c31e9cc0512eece1c87520fa
Maddie <maddieeckstrom@gmail.com>
2023-09-12 09:19:01 -1000

    added header to html and some css styling

--- Assets/script.js
+++ Assets/script.js
@@ -1,6 +1,19 @@
 var APIKey = "3ed399bdb9a0f567232019fc606b44b6";
 var city;
+
+function getCurrentWeather() {
 // Stores the OpenWeather Current Weather Data URL and the necessary variables
 var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
 // will need to adjust application to accept user input, to store in the city variable that I created
-fetch(queryURL);
+fetch(queryURL)
+    .then(function (response) {
+        return response.json();
+    })
+
+
+
+    .catch(function(error) {
+        console.log(error);
+    });
+
+}
