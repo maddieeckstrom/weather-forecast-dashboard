03300d51f4c59917bbed94326746ebe2bce8cf97
Maddie <maddieeckstrom@gmail.com>
2023-09-12 14:34:16 -1000

    added boxes for current and future weather

--- Assets/script.js
+++ Assets/script.js
@@ -1,19 +1,26 @@
-var APIKey = "3ed399bdb9a0f567232019fc606b44b6";
-var city;
+const APIKey = "3849ccbbe5a892256073c223a4de1590";
+let city = document.getElementById("cityInput");
 
 function getCurrentWeather() {
 // Stores the OpenWeather Current Weather Data URL and the necessary variables
-var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+//var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+
+var queryURL = "https://api.openweathermap.org/data/2.5/weather?zip=96786,us&appid=3849ccbbe5a892256073c223a4de1590";
+
+
 // will need to adjust application to accept user input, to store in the city variable that I created
 fetch(queryURL)
     .then(function (response) {
         return response.json();
     })
-
-
+    .then(function (data) {
+        console.log(data);
+    })
 
     .catch(function(error) {
         console.log(error);
     });
 
 }
+
+getCurrentWeather();
